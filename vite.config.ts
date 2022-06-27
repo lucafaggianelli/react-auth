124e598505f466d78d5557fe9630b771fd624f17
Luca Faggianelli <luca.faggianelli@gmail.com>
2022-06-27 12:18:48 +0200

    Vite setup for React lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import path from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.ts'),
+      name: 'React Auth',
+      fileName: (format) => `react-auth.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        }
+      }
+    }
+  }
 })
